5a9e51ec158cb27b4cc0cb509ec60bf764079f76
David McCullough <dmccullough488@gmail.com>
2025-11-20 22:21:06 -0600

    fixed issue

--- Menu/MenuButton.jsx
+++ Menu/MenuButton.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Button from "../src/Button/Button"
+import Button from "../Button/Button"
 
 export default function MenuButton({ buttonText, onClick }) {
   return (
