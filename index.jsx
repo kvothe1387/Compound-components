8e1a2f0c194b1f46dd7514d09e5c0e10eee73f53
David McCullough <dmccullough488@gmail.com>
2025-11-20 23:54:49 -0600

    created ne MenuItem component.
    Map over sports [] inside MenuDropdown and render a MenuItem for each sport in the []

--- index.jsx
+++ index.jsx
@@ -3,26 +3,29 @@ import ReactDOM from 'react-dom/client';
 import Menu from "./Menu/Menu";
 import MenuButton from './Menu/MenuButton';
 import MenuDropdown from './Menu/MenuDropdown';
+import MenuItem from './Menu/MenuItem';
 
 /**
- * Challenge:
- * 1. Convert the MenuButton to accept children and 
- *    render them. (In this case, the children will
- *    just be the button text)
- * 2. Change the MenuButton below to pass "Sports"
- *    in as a child of the component instead of by
- *    using the `buttonText` prop.
- * 
- * NOTE: It's fine that the menu is still broken, I
- * promise we're getting there! 😃
- */
+   * 1. MenuDropdown should render children instead of items
+   * 2. MenuItem (new component you need to create) should also
+   *    render children. Grab the <div className="menu-item">
+   *    from MenuDropdown before deleting it if you want to be
+   *    lazy :)
+   * 3. Map over the `sports` array inside MenuDropdown in this
+   *    file and render a MenuItem for each sport in the array
+   */
 
 function App() {
+  const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"]
+
   return (
     <Menu>
       <MenuButton>Sports</MenuButton>
-      <MenuDropdown items={["Tennis", "Pickleball", "Racquetball", "Squash"]} />
-
+      <MenuDropdown>
+        {sports.map(sport => (
+          <MenuItem key={sport}>{sport}</MenuItem>
+        ))}
+      </MenuDropdown>
     </Menu>
   )
 }
