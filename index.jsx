bb53230634191313b63b22459bbc64a56c097248
David McCullough <dmccullough488@gmail.com>
2025-11-26 20:50:11 -0600

    Created ne MenuItem so MenuDropdown will render children.
    Mapped over the `sports` array and render a MenuItem for each sport in the array

--- index.jsx
+++ index.jsx
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 import Menu from "./Menu/Menu"
 import MenuButton from './Menu/MenuButton';
 import MenuDropdown from './Menu/MenuDropdown';
+import MenuItem from './Menu/MenuItem';
 
 function App() {
+  const sports = ["Tennis", "Racquetball", "Pickleball", "Squash"]
+
   return (
 
     <Menu>
       <MenuButton>Sports</MenuButton>
-      <MenuDropdown items={["Tennis", "Racquetball", "Pickleball", "Squash"]} />
-    </Menu>
-
+      <MenuDropdown>
+        {sports.map(sport => (
+          <MenuItem key={sport}>{sport}</MenuItem>
+        ))}
+      </MenuDropdown>
+    </Menu >
   )
 }
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
