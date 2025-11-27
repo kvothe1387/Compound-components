fe0207885d83271ed180de0e91f7914c790bde75
David McCullough <dmccullough488@gmail.com>
2025-11-26 21:51:08 -0600

    made A11y with aria, role, and id lables

--- Menu/Menu.jsx
+++ Menu/Menu.jsx
@@ -6,14 +6,15 @@ const MenuContext = React.createContext()
 
 export default function Menu({ children }) {
   const [open, setOpen] = React.useState(false)
+  const menuId = React.useId()
 
   function toggle() {
     setOpen(prevOpen => !prevOpen)
   }
 
   return (
-    <MenuContext.Provider value={{ open, toggle }}>
-      <div className="menu">
+    <MenuContext.Provider value={{ open, toggle, menuId }}>
+      <div className="menu" role="menu">
         {children}
       </div>
     </MenuContext.Provider>
--- Menu/MenuButton.jsx
+++ Menu/MenuButton.jsx
@@ -3,8 +3,15 @@ import Button from "../Button/Button.jsx"
 import { MenuContext } from "./Menu.jsx"
 
 export default function MenuButton({ children }) {
-  const { toggle } = React.useContext(MenuContext)
+  const { toggle, open, menuId } = React.useContext(MenuContext)
   return (
-    <Button onClick={toggle}>{children}</Button>
+    <Button
+      onClick={toggle}
+      aria-expanded={open}
+      aria-haspopup="true"
+      aria-controls={menuId}
+    >
+      {children}
+    </Button>
   )
 }
